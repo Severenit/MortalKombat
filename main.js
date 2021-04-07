d12481c436ffeb313f55e528c4404e1066b46733
Zar Zakharov <severenit@gmail.com>
2021-04-07 10:48:12 +0300

    HomeWork-2
    Done

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-console.log('' + 5);
-console.log(+'10');
-console.log(!!'someString');
+const $player1 = document.createElement('div');
+$player1.classList.add('player1');
+
+const $p = document.createElement('p');
+$p.innerText = 'Scorpion';
+
+const $img = document.createElement('img');
+$img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+$player1.appendChild($p);
+$player1.appendChild($img);
+
+const $root = document.querySelector('.root');
+$root.appendChild($player1);
