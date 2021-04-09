1b3b17ffb0dfa4e21cb33d6b3dd907b4f92ed112
Zar Zakharov <severenit@gmail.com>
2021-04-09 08:21:27 +0300

    DONE

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-const $player1 = document.createElement('div');
-$player1.classList.add('player1');
+const player1 = {
+    name: 'SCORPION',
+    hp: 50,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    attack: function(name) {
+        console.log(name + ' ' + 'Fight...');
+    },
+}
+const player2 = {
+    name: 'LIU KANG',
+    hp: 80,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+    attack: function(name) {
+        console.log(name + ' ' + 'Fight...');
+    },
+}
 
-const $p = document.createElement('p');
-$p.innerText = 'Scorpion';
+function createPlayer(classPlayer, playerObj) {
+    const $arenas = document.querySelector('.arenas');
+    const $player = document.createElement('div');
+    const $progressbar = document.createElement('div');
+    const $character = document.createElement('div');
+    const $life = document.createElement('div');
+    const $name = document.createElement('div');
+    const $img = document.createElement('img');
 
-const $img = document.createElement('img');
-$img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+    $player.classList.add(classPlayer);
+    $progressbar.classList.add('progressbar');
+    $character.classList.add('character');
+    $life.classList.add('life');
+    $name.classList.add('name');
 
-$player1.appendChild($p);
-$player1.appendChild($img);
+    $life.style.width = playerObj.hp +'%';
+    $name.innerText = playerObj.name;
+    $img.src = playerObj.img;
 
-const $root = document.querySelector('.root');
-$root.appendChild($player1);
+    $progressbar.appendChild($name);
+    $progressbar.appendChild($life);
+
+    $character.appendChild($img);
+
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+
+    $arenas.appendChild($player);
+}
+
+createPlayer('player1',player1);
+createPlayer('player2',player2);
