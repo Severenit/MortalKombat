a709d732be99fde39792b14134c53107649c13a1
Zar Zakharov <severenit@gmail.com>
2021-04-20 21:20:01 +0300

    FIX

---
+++ player.js
@@ -0,0 +1,14 @@
+export const player = {
+    player: 1,
+    name: 'SCORPION',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+}
+
+export const playerNew = {
+    player: 1,
+    name: 'SCORPION',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+}
+
