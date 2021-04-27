3408b7dd268714ee0b7ff962f3e2ef5e07b11c1a
Zar Zakharov <severenit@gmail.com>
2021-04-27 14:37:57 +0300

    FINISH

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getRandom, createElement, getTime } from '/utils2.js';
+import { getRandom, createElement, getTime } from './utils2.js';
 import { HIT, LOGS, ATTACK } from './constants/index.js';
 import Player from './Player/index.js';
 
